fb43563106c3ab644563cb0e41af22e932d12771
Daksha-97 <charanthusaidaksha@gmail.com>
2025-07-25 03:10:28 +0530

    Re-create missing main application file

---
+++ src/main/java/com/example/smartquiz/SmartquizApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartquizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartquizApplication.class, args);
+    }
+
+}
